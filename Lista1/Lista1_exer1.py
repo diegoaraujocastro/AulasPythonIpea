9119136dba7670696ece570ac0cb9ed1f66f0f8c
diegoaraujocastro <diegoaraujocastro@gmail.com>
2020-06-15 23:24:42 -0300

    Montando a base

---
+++ Lista1/Lista1_exer1.py
@@ -0,0 +1,13 @@
+def print_sentences():
+    a= '*'
+    b= '* '
+    print(a)
+    print(b*2)
+    print(b*3)
+    print(b*4)
+    print(b*3)
+    print(b*2)
+    print(a)
+
+if __name__ == '__main__':
+    print_sentences()
